c0a8eac4b23310a740724e13b5c5c2f2b0daa2a7
Ana Ascua <anaascua5@yahoo.com.ar>
2023-09-16 19:42:46 -0300

    agrego nodemon, mercadopago, tokens (hidden)

--- JavaScript 4/client/js/cart.js
+++ JavaScript 4/client/js/cart.js
@@ -92,9 +92,68 @@ const displayCart = () => {
     modalFooter.className = "modal-footer";
     modalFooter.innerHTML = `
       <div class="total-price">TOTAL: ${total} $</div>
+      <button class="btn-primary" id="checkout-btn"> go to checkout</button>
+      <div id="button-checkout"></div>
     `;
 
     modalContainer.append(modalFooter);
+    //mercadopago;
+    const mercadopago = new MercadoPago("TEST-numeroLargoConLetrasAlFinal", {
+      locale: "es-AR", // los mas comunes son pt-BR, es-AR, en-US
+    });
+
+    const checkoutButton = modalFooter.querySelector("#checkout-btn");
+
+    checkoutButton.addEventListener("click", function () {
+      checkoutButton.remove();
+
+      const orderData = {
+        quantity: 1,
+        description: "compra de ecommerce",
+        price: total,
+      };
+
+      fetch("http://localhost:8080/create_preference", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(orderData),
+      })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (preference) {
+        createCheckoutButton(preference.id);
+        })
+      .catch(function () {
+        alert("Unexpected error");
+      });
+    });
+
+    function createCheckoutButton(preferenceId) {
+      // Initiailize the checkout
+      const bricksBuilder = mercadopago.bricks();
+
+      const renderComponent = async (bricksBuilder) => {
+        // if (window.checkoutButton) checkoutButton.unmount();
+
+        await bricksBuilder.create(
+          "wallet",
+          "button-checkout", // class/id where the payment button will be displayed
+          {
+            initialization: {
+              preferenceId: preferenceId,
+            }, 
+            callbacks: {
+              onError: (error) => console.error(error),
+              onReady: () => {},
+            },
+          }
+        );
+      };
+      window.checkoutButton = renderComponent(bricksBuilder);
+    }
   } else {
     // Si el carrito está vacío, mostrar un mensaje en el modal
     const modalText = document.createElement("h2");
