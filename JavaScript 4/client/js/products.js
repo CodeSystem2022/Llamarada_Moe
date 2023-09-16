157a66309fd87e9a25eecaee2f5f9cb5beb87826
Ana Ascua <anaascua5@yahoo.com.ar>
2023-09-16 19:48:02 -0300

    hago cambios en direcciones en archivos .js, index

--- JavaScript 4/client/js/products.js
+++ JavaScript 4/client/js/products.js
@@ -4,55 +4,55 @@ const productos = [
         productName: "Banana",
         price: 480,
         quantity: 1,
-        img: "media/bananas.png",
+        img: "/client/media/bananas.png",
     },
     {
         id: 2,
         productName: "Hamburguesas",
         price: 1200,
         quantity: 1,
-        img: "media/hamburguesas.png",
+        img: "/client/media/hamburguesas.png",
     },
     {
         id: 3,
         productName: "Facturas",
         price: 1700,
         quantity: 1,
-        img: "media/facturas.jpg",
+        img: "/client/media/facturas.jpg",
     },
     {
         id: 4,
         productName: "Mayonesa 500 Ml",
         price: 384,
         quantity: 2,
-        img: "media/Aderezo-Mayonesa-Natura-475-Gr-1-9501.webp",
+        img: "/client/media/Aderezo-Mayonesa-Natura-475-Gr-1-9501.webp",
     },
     {
         id: 5,
         productName: "Gomitas",
         price: 380,
         quantity: 1,
-        img: "media/gomitas.png",
+        img: "/client/media/gomitas.png",
     },
     {
         id: 6,
         productName: "Nesquik",
         price: 690,
         quantity: 10,
-        img: "media/nesquik.png",
+        img: "/client/media/nesquik.png",
     },
     {
         id: 7,
         productName: "Pan de panchos",
         price: 403,
         quantity: 1,
-        img: "media/pan-de-panchos.webp",
+        img: "/client/media/pan-de-panchos.webp",
     },
     {
         id: 8,
         productName: "Vino",
         price: 1500,
         quantity: 1,
-        img: "media/vino.jpg"
+        img: "/client/media/vino.jpg"
     }
 ]
