beec71d0644848543b1350966c96885c9294074a
Ana Ascua <anaascua5@yahoo.com.ar>
2023-09-19 11:01:52 -0300

    Python: Sistemas de numeración y diferentes tipos

---
+++ Python-4/Clase01/Sistemas_numeracion.py
@@ -0,0 +1,45 @@
+# Profundizando en los sistemas de numeración
+# 2.1 Comenzamos con -> Decimal, Binario, Octal y Hexadecimal
+
+# Sistema decimal
+a = 10
+print(f'a: decimal {a}')
+
+# Sistema binario
+a = 0b1010
+print(f'a: binario {a}')
+
+# Sistema octal (8)
+a = 0o12
+print(f'a: octal {a}')
+
+# Sistema hexadecimal
+a = 0xA
+print(f'a: hexadecimal {a}')
+
+
+# 2.2 Seguimos con -> Decimal, Binario, Octal y Hexadecimal. Ahora con int('valor', base)
+# Base Decimal -> (10)
+a = int('23', 10)
+print(f'a: base decimal {a}')
+
+# Base Binario -> (2)
+a = int('10111', 2)
+print(f'a: base binaria {a}')
+
+
+# 2.2 parte video 2
+# Base Octal -> (8)
+a = int('27', 8)
+print(f'a = base octal: {a}')
+
+# Base Hexadecimal
+a = int('17', 16)
+print(f'a = base hexadecimal: {a}')
+
+# Base 5 (sus valores son de 0 a 4)
+a = int('344', 5)
+print(f'a = Base 5: {a}')
+
+
+# 2.3 Tipo float como constructor y comienzo de exponencial
---
+++ Python-4/Clase01/valores_infinitos.py
@@ -0,0 +1,36 @@
+# 2.5 Valor Infinito Positivo y negativo
+# video 6
+
+import math
+from decimal import Decimal
+
+# manejo de valores infinitos
+infinito_positivo = float('inf')
+print(f'Infinito positivo: {infinito_positivo}')
+print(f'Es infinito?: {math.isinf(infinito_positivo)}')
+
+infinito_negativo = float('-inf')
+print(f'Infinito negativo: {infinito_negativo}')
+print(f'Es infinito?: {math.isinf(infinito_negativo)}')
+
+
+# 2.5 Valor Infinito Positivo y negativo
+# video 7
+
+# Módulo math
+infinito_positivo = math.inf
+print(f'Infinito positivo: {infinito_positivo}')
+print(f'Es infinito?: {math.isinf(infinito_positivo)}')
+
+infinito_negativo = -math.inf
+print(f'Infinito negativo: {infinito_negativo}')
+print(f'Es infinito?: {math.isinf(infinito_negativo)}')
+
+# Módulo decimal
+infinito_positivo = Decimal('Infinity')
+print(f'Infinito positivo: {infinito_positivo}')
+print(f'Es infinito?: {math.isinf(infinito_positivo)}')
+
+infinito_negativo = Decimal('-Infinity')
+print(f'Infinito negativo: {infinito_negativo}')
+print(f'Es infinito?: {math.isinf(infinito_negativo)}')
