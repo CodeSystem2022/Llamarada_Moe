dfb1109010894ed8329c3d76f6d55ea28c3584e4
aniascua <83845164+aniascua@users.noreply.github.com>
2023-10-18 20:45:15 -0300

    JAVA: Clase 10, continuamos con la carga del libro seleccionado
    private void cargarLibroSeleccionado() { ... }

--- JAVA-4/Lección03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroForm.java
+++ JAVA-4/Lección03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroForm.java
@@ -78,6 +78,20 @@ public class LibroFrom extends JFrame {
         var renglon = tablaLibros.getSelectedRow();
         if(renglon != -1) {
             String idLibro = tablaLibros.getModel().getValueAt(renglon, 0).toString();
+            idTexto.setText(idLibro); // obtenemos el ID para el libro en el JTextField (oculto x ahora)
+            String nombreLibro =
+                    tablaLibros.getModel().getValueAt(renglon, 1).toString();
+            libroTexto.setText(nombreLibro);
+            String autor =
+                    tablaLibros.getModel().getValueAt(renglon, 2).toString();
+            autorTexto.setText(autor);
+            String precio =
+                    tablaLibros.getModel().getValueAt(renglon, 3).toString();
+            precioTexto.setText(precio);
+            String existencias =
+                    tablaLibros.getModel().getValueAt(renglon, 4).toString();
+            existenciaTexto.setText(existencias);
+
         }
     }
 
